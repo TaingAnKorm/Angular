de77454d4e8a5f45ea1df8b7abe80e6c7092a146
TaingAnKorm <kormtaingan99@gmail.com>
2025-08-14 14:23:46 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Dashboard } from './dashboard/dashboard';
+import { Category } from './category/category';
 
 export const routes: Routes = [
-{path:'',component:Home},
-{path:'dashboard',component:Dashboard}
+    {path:'',component:Home},
+    {path:'home',component:Home},
+    {path:'dashboard',component:Dashboard},
+    {path:'category',component:Category},
 ];
