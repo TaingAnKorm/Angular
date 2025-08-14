69d7814c5a9ea7dcb0ac07042a5f1862e213dd55
TaingAnKorm <kormtaingan99@gmail.com>
2025-08-14 14:28:21 +0700

    update

--- src/app/category/category.ts
+++ src/app/category/category.ts
@@ -4,8 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-category',
   imports: [],
   templateUrl: './category.html',
-  styleUrl: './category.css'
+  styleUrl: './category.css',
 })
 export class Category {
-
+  lsCatagary = [
+    { id: 1, category: 'samsung', remark: 'Make in ko' },
+    { id: 2, category: 'LG', remark: 'Make in JP' },
+    { id: 3, category: 'Steing', remark: 'Make in Cam' },
+    { id: 4, category: 'ABA', remark: 'Make in KH' },
+  ];
 }
